5219f1e69b9a5e004e529dd1e8e4e94a73a0e218
Filip Jaksic <filipjksc@gmail.com>
2024-04-09 20:50:46 +0200

    MVP: Generate dummy Viper program

--- Encoder/Encoder.hs
+++ Encoder/Encoder.hs
@@ -23,5 +23,23 @@ encodeexpr expr = undefined
 encodeprogram :: Cell -> VMember
 encodeprogram cell = undefined
 
+
+-- Cells can depend on other cells, so encode them in a graph like way to construct the
+-- Viper prog. Like from leaf to root cells, where leaf cells are cells without a
+-- dependency.
 encode :: Spreadsheet -> VProgram
-encode sheet = undefined
+encode sheet = VProgram [mainMethod] preludeString
+  where
+    preludeString = ""
+    mainMethod = VMethod "mainMethod" args argsDecl requiresExpr ensuresExpr (Just (VSeq statements))
+      where
+        args = []
+        argsDecl = []
+        requiresExpr = []
+        ensuresExpr = []
+        statements = [VComment "my amazing comment"]
+
+-- WIP: just encode each cell as it's own program and concat them
+-- encode sheet = concatMap concatRow sheet
+--   where
+--     concatRow row = concatMap encodeprogram row
